6270bee85bd3ced996e6069fb91e26d11bad3a2c
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-12-06 02:44:41 +0800

    Implement Greyscale & Inversion

--- ImageProcessor/ImageProcessor/Libraries/ImageProcessLib.cs
+++ ImageProcessor/ImageProcessor/Libraries/ImageProcessLib.cs
@@ -1,13 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace ImageProcessor
 {
     public class ImageProcessLib
     {
+        public static void Greyscale(ref Bitmap loaded, ref Bitmap processed)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            for(int i = 0; i < loaded.Width; i++)
+            {
+                for(int j = 0; j < loaded.Height; j++)
+                {
+                    Color pixel = loaded.GetPixel(i, j);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    Color greyscale = Color.FromArgb(grey, grey, grey);
+                    processed.SetPixel(i, j, greyscale);
+                }
+            }
+        }
 
+        public static void Inversion(ref Bitmap loaded, ref Bitmap processed)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    Color pixel = loaded.GetPixel(i, j);
+                    Color inversion = Color.FromArgb(255 - pixel.R, 255 - pixel.G, 255 - pixel.B) ;
+                    processed.SetPixel(i, j, inversion);
+                }
+            }
+        }
     }
 }
