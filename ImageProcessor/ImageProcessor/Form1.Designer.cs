12340584c6fd24f7a8ba0c95914cfead58bfeaca
Carl Sebastian Yebes <carlsebastian.yebes@cit.edu>
2022-12-06 01:06:41 +0800

    Change form name

--- ImageProcessor/ImageProcessor/Form1.Designer.cs
+++ ImageProcessor/ImageProcessor/Form1.Designer.cs
@@ -36,6 +36,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
+            this.Text = "Image Processor";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- ImageProcessor/ImageProcessor/Form1.cs
+++ ImageProcessor/ImageProcessor/Form1.cs
@@ -16,5 +16,10 @@ namespace ImageProcessor
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
